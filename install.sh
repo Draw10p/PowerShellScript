039a1df7f0cc8f2ca792a778914bcb166d3737f7
Draw10p <daromanag@uide.edu.ec>
2025-12-08 14:46:38 +0000

    Trabajo en Clase.2 8 -DIC-2025

--- install.sh
+++ install.sh
@@ -68,23 +68,3 @@ sudo apt-get install -y powershell
 pwsh
 
 # pwsh → ejecutable de PowerShell 7+ (inicia la consola de PowerShell)
-
-function Start-ProgressBar {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true)]
-        $Title,
-        
-        [Parameter(Mandatory = $true)]
-        [int]$Timer
-    )
-    
-    for ($i = 1; $i -le $Timer; $i++) {
-        Start-Sleep -Seconds 1
-        $percentComplete = ($i / $Timer) * 100
-        Write-Progress -Activity $Title -Status "$i seconds elapsed" -PercentComplete $percentComplete
-    }
-} 
-
-# Call the function
-Start-ProgressBar -Title "Test timeout" -Timer 30
